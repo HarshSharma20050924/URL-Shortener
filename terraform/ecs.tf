da6a1dec05bc6f51f778acffc7f9a1e52d4951af
Harsh <harsh20050924@gmail.com>
2026-04-01 00:02:43 +0530

    fix: Enable outbound traffic for ECS tasks

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -48,6 +48,13 @@ resource "aws_security_group" "app_sg" {
     from_port   = 5000
     to_port     = 5000
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # Should be restricted to LB in real prod
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
