15e7fd123fd2489b057d4b643c480a333b2f3c9f
Harsh <harsh20050924@gmail.com>
2026-04-01 01:17:05 +0530

    fix: Add public route table to connect ALB to Internet Gateway

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -28,4 +28,23 @@ resource "aws_internet_gateway" "gw" {
   vpc_id = aws_vpc.main.id
 }
 
+# Route Table for public subnets - routes traffic to Internet Gateway
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = { Name = "url-shortener-public-rt" }
+}
+
+# Associate the route table with each public subnet
+resource "aws_route_table_association" "public" {
+  count          = 2
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
+
 data "aws_availability_zones" "available" {}
